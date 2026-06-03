7ae81ad08ca0544b4d5cf5fc408a2bfa67ce6f22
gauravsingh41 <gauravsinghok7@gmail.com>
2026-06-03 19:39:01 +0530

    day3 assignment is uploaded

---
+++ day3/q2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end;
+    cout << "Enter start of range: ";
+    cin >> start;
+    cout << "Enter end of range: ";
+    cin >> end;
+
+    for (int n = start; n <= end; n++) {
+        if (n <= 1) continue;
+        bool prime = true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) cout << n << " ";
+    }
+    return 0;
+}
+
---
+++ day3/q4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    int max = (a > b) ? a : b;
+    while (true) {
+        if (max % a == 0 && max % b == 0) {
+            cout << "LCM: " << max;
+            break;
+        }
+        ++max;
+    }
+    return 0;
+}   
