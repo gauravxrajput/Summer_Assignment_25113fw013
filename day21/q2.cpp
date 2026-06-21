fec7611ddb4812f8f357a504df8932435656a0ed
gauravsingh41 <gauravsinghok7@gmail.com>
2026-06-21 21:10:33 +0530

    day21 assignment is uploaded

---
+++ day21/q2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int start = 0;
+    int end = strlen(str) - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+
+    cout << "Reversed string: " << str << endl;
+    return 0;
+}
